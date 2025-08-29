6ae04bab839fe46f488d1f72e0fc463f4c478c91
Anand Narwade <126019417+anandnarwade7@users.noreply.github.com>
2025-08-29 13:26:56 +0530

    change favicon and resposiveness

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ function App() {
       {/* Hero */}
       <div
         id="hero"
-        className="min-h-screen relative overflow-hidden bg-gray-900 pt-10"
+        className="relative overflow-hidden bg-gray-900 pt-10"
       >
         {/* Background Pattern */}
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#080808_1px,transparent_1px),linear-gradient(to_bottom,#080808_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
